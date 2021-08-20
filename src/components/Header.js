c621102e808bac0d8a02feaeddfd702de7664fd8
Charlie Martinez <caramartinezrio@unal.edu.co>
2021-08-20 14:29:30 -0500

    Header update. (3D canvas is missing)

---
+++ src/components/Header.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+// Assets
+import iconFacebook from "../assets/icons/facebook.svg";
+
+// Styles
+import "./Variables.css";
+import "./Header.css";
+
+export default function Header(props) {
+  return (
+    <div className="header-container">
+      <div className="header-container__login">
+        <div className="inputs-login">
+          <div className="inputs-login__inputs">
+            <input
+              type="text"
+              name="inputEmail"
+              id="inputEmail"
+              placeholder="Email"
+            />
+            <input
+              type="text"
+              name="inputPassword"
+              id="inputPassword"
+              placeholder="Password"
+            />
+          </div>
+          <div className="inputs-login__btn">
+            <span>let´s go!</span>
+          </div>
+        </div>
+        <div className="social-network">
+          <div className="social-network__icon">
+            <img src={iconFacebook} alt="Icono de facebook" />
+          </div>
+          <div className="social-network__text">
+            <span>Login with Facebook so you can play with your friends.</span>
+            <span>More ways to login</span>
+          </div>
+        </div>
+      </div>
+      <div className="header-container__info">
+        <div className="nautilus"></div>
+        <div className="info">
+          <div className="container-info">
+            <div className="container-info__title">
+              <h2>MAKE FRIENDS & CHAT WITH MILLIONS IN A VIRTUAL WORLD</h2>
+            </div>
+            <div className="container-info__button">
+              <span
+                onClick={() => {
+                  props.setRegisterModal(true);
+                }}
+              >
+                JOIN FOR FREE
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Landing.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Landing = (props) => {
+  return (
+    <>
+      <Header props={props} />
+
+      <Footer />
+    </>
+  );
+};
+
+export default Landing;
