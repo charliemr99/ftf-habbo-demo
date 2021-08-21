85a1b3cc02aa167d94e775f5fda7eacaab8ac282
Charlie Martinez <caramartinezrio@unal.edu.co>
2021-08-21 10:15:02 -0500

    Nautilus canvas + Resize and setup functions

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,82 @@
 import React from "react";
+import { APP } from "../js/nautilus/app";
 
 // Assets
 import iconFacebook from "../assets/icons/facebook.svg";
+import habboLogo from "../assets/icons/habbo_logo.svg";
+import landingRoom from "../assets/landingRoom.json";
 
 // Styles
 import "./Variables.css";
 import "./Header.css";
 
 export default function Header({ setRegisterModal }) {
+  const [resize, setResize] = React.useState(false);
+  let player = null;
+  const roomContainer = React.useRef();
+
+  function onWindowResize(event) {
+    if (window.screen.width > 320 && window.screen.width < 768) {
+      setResize(true);
+    }
+    resize3DRoom();
+  }
+
+  function setUp3DRoom() {
+    player = new APP.Player();
+    player.load(landingRoom, {});
+    player.setSize(
+      roomContainer.current.clientWidth,
+      roomContainer.current.clientHeight
+    );
+    player.play({ data: {} });
+
+    roomContainer.current.appendChild(player.dom);
+  }
+
+  function resize3DRoom() {
+    if (player) {
+      console.log("player,", player);
+
+      player.setSize(
+        roomContainer.current.clientWidth,
+        roomContainer.current.clientHeight
+      );
+
+      player.getCamera().updateProjectionMatrix();
+    }
+    setResize(false);
+  }
+
+  function remove3DRoom() {
+    if (player) {
+      roomContainer.current.removeChild(player.dom);
+      player.stop();
+    }
+  }
+
+  React.useEffect(() => {
+    window.addEventListener("resize", onWindowResize);
+    setUp3DRoom();
+    return () => {
+      window.removeEventListener("resize", onWindowResize);
+      remove3DRoom();
+    };
+  }, [resize]);
+
+  React.useEffect(() => {
+    setResize(true);
+    setTimeout(() => {
+      setResize(false);
+    }, 1000);
+  }, []);
+
   return (
     <div className="header-container">
       <div className="header-container__login">
+        <div className="inputs-login__responsive">
+          <span>Login</span>
+        </div>
         <div className="inputs-login">
           <div className="inputs-login__inputs">
             <input
@@ -40,8 +106,13 @@ export default function Header({ setRegisterModal }) {
           </div>
         </div>
       </div>
+      <img className="habbo-logo" src={habboLogo} alt="Habbo Logo" />
       <div className="header-container__info">
-        <div className="nautilus"></div>
+        <div
+          className="nautilus"
+          ref={roomContainer}
+          style={{ width: "50vw", minHeight: "600px" }}
+        ></div>
         <div className="info">
           <div className="container-info">
             <div className="container-info__title">
